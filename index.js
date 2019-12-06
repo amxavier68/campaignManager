a0c5e7108955e2ade910a316042c4e6802a314b6
Anthony Xavier <anthony-xavier@live.com>
2019-12-06 11:48:17 +1000

    c

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({Action: '1st Route loaded ...'})
+    res.send({Good: 'Bye ...'})
 });
 
 const PORT = process.env.PORT || 5000;
