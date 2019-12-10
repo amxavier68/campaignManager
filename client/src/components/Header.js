0ff1d5e8ea1e66c03e329d4a224fb8489a0412f4
Anthony Xavier <anthony-xavier@live.com>
2019-12-10 12:39:11 +1000

    Logout functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Header extends Component {
-    render() {
+
+  renderContent() {
+    switch(this.props.auth){
+      case null:
+        return;
+      case false: 
         return (
-            <nav>
-            <div className="nav-wrapper">
-              <a href="/" className="left brand-logo">Emaily</a>
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                  <li><a href="/auth/google">Login with Google</a></li>
-              </ul>
-            </div>
-          </nav>
-        )
+          <li><a href="/auth/google">Login with Google</a></li>
+        );
+      default:
+        return (
+          <li><a href="/api/logout">Logout</a></li>
+        );
     }
+  }
+
+  render() {
+      return (
+          <nav>
+          <div className="nav-wrapper">
+            <a href="/" className="left brand-logo">Emaily</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                { this.renderContent() }
+            </ul>
+          </div>
+        </nav>
+      )
+  }
 }
 
-export default Header;
+function mapStateToProps({auth}){
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,13 +8,21 @@ module.exports = app => {
     scope: ['profile', 'email']
     })
   );
+  
   // Pass returned client code back to Google OAuth for usage
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    });
+
   // Logout function
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   });
+  
   // Lists current user
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
