fc03cfca62acb07e2789baab317f6bdd4e60beba
Anthony Xavier <anthony-xavier@live.com>
2019-12-11 09:30:24 +1000

    Charges

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookie = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 require('./models/User');
 require('./services/passport');
 
@@ -10,6 +11,8 @@ const keys = require('./config/keys');
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(cookie({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -20,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ routes/billingRoutes.js
@@ -0,0 +1,18 @@
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.StripeSecretKey);
+
+module.exports = app => {
+
+  // Stripe integration
+  app.post('/api/stripe', async (req, res) => {
+    //   console.log(req.body);
+    const charge = await stripe.charges.create({
+        amount: 500,
+        currency: 'aud',
+        description: 'Emaily - $5 for 5 credits',
+        source: req.body.id
+    });
+    console.log(charge);
+  });
+
+};
