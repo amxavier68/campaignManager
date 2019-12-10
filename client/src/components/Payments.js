399edd68278e88fc14975e1ed0f676f555587b42
Anthony Xavier <anthony-xavier@live.com>
2019-12-10 17:50:15 +1000

    c

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -4,15 +4,15 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class Payments extends Component {
-    render() {
-        return (
-            <StripeCheckout name="Emaily" description="$5 for 5 email credits"
-              amount={500} currency="AUD"
-              stripeKey={process.env.REACT_APP_STRIPE_KEY}
-              token={token => this.props.handleToken(token)}
-            />
-        );
-    }
+  render() {
+    return (
+        <StripeCheckout name="Emaily" description="$5 for 5 email credits"
+            amount={500} currency="AUD"
+            stripeKey={process.env.REACT_APP_STRIPE_KEY}
+            token={token => this.props.handleToken(token)}
+        />
+    );
+  }
 }
 
 export default connect(null, actions)(Payments);
