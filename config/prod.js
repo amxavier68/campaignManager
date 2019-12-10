9d1e481476229f4c4dbffaa7a0aa1bd641d80f92
Anthony Xavier <anthony-xavier@live.com>
2019-12-10 14:50:57 +1000

    Pre env file implementation

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
+    StripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    StripeSecretKey: process.env.STRIPE_SECRET_KEY,
     cookieKey: process.env.COOKIE_KEY
 };
