efd857c553b5bfab8473cd8e59f3891d392997d6
Anthony Xavier <anthony-xavier@live.com>
2019-12-13 18:42:55 +1000

    c

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -6,13 +6,13 @@ module.exports = app => {
   // Stripe integration
   app.post('/api/stripe', async (req, res) => {
 
-    // console.log(req.body);
-    // console.log(req.body.id);
+    console.log(req.body);
+    console.log(req.body.id);
     
     const charge = await stripe.charges.create({
-        amount: 500,
+        amount: 1495,
         currency: "aud",
-        description: "Emaily - $5 for 5 credits",
+        description: "Emaily - $14.95 for 5 credits",
         source: req.body.id
     }).catch((err) => console.log(err.message));
     
