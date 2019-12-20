ed55193a703cc3e28b53f61805e058c06823ee8a
Anthony Xavier <anthony-xavier@live.com>
2019-12-20 14:50:18 +1000

    C

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,9 +1,28 @@
-const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-export default (emails) => {
+const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default emails => {
   const invalidEmails = emails
     .split(',')
     .map(email => email.trim())
     .filter(email => re.test(email) === false);
   return (invalidEmails.length) ? `Invalid Emails: ${invalidEmails}` : null;
 };
+
+
+
+ 
+// export default emails => {
+//   const invalidEmails = emails
+//     .split(",")
+//     .map(email => email.trim())
+//     .filter(email => re.test(email) === false);
+ 
+//   if (invalidEmails.length) {
+//     const addSpacing = invalidEmails.map(email => " " + email);
+//     return `These emails are invalid:${addSpacing}`;
+//   }
+ 
+//   return;
+// };
